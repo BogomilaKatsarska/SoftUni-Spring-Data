cb4b65c418e2cf2fc7ea73adc9b05edac882550b
Bogomila Katsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2024-11-28 13:07:57 +0200

    Add files via upload

---
+++ 04.HibernateCodeFirst/universitySystem/Student.java
@@ -0,0 +1,23 @@
+package bg.softuni.universitySystem;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "students")
+public class Student extends Person {
+    @Column(name = "average_grade")
+    private double averageGrade;
+
+    @Basic
+    private int attendance;
+
+    @ManyToMany(mappedBy = "students")
+    private List<Course> courses;
+
+    public Student() {
+        super();
+    }
+
+}
